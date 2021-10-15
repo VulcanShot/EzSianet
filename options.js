18a8ee834e4665941ef0112245b2afb4bdaef01b
VulcanShot <pieromarcelohp@gmail.com>
2021-10-15 09:58:49 -0500

    deleted options page

--- options.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('save-button').addEventListener('click', SetLink);
-
-function SetLink() {
-    var url = document.getElementById('link').value;
-    console.log('URL updated manually: ', url)
-    chrome.storage.sync.set({link: url});
-}
