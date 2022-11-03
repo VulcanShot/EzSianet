04d4b519a59ca3ec07e0f9e915e27a0e422353c6
VulcanShot <pieromarcelohp@gmail.com>
2022-11-02 19:09:39 -0500

    Background: Simplified event listeners

--- src/background.js
+++ src/background.js
@@ -1,17 +1,8 @@
 chrome.runtime.onInstalled.addListener(details => { OnInstallation(details) });
-chrome.tabs.onCreated.addListener(ListenToWebRequests)
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status != 'complete') return
-    ListenToWebRequests();
-});
-
-function ListenToWebRequests() {
-    chrome.webRequest.onCompleted.removeListener(CheckWebRequests);
-    chrome.webRequest.onCompleted.addListener(
-        CheckWebRequests,
-        { urls: ["*://www.sianet.edu.pe/*"] } //https://developer.chrome.com/docs/extensions/mv3/match_patterns/
-    );
-}
+chrome.webRequest.onCompleted.addListener(
+    CheckWebRequests,
+    { urls: ["*://www.sianet.edu.pe/*"] } //https://developer.chrome.com/docs/extensions/mv3/match_patterns/
+);
 
 function CheckWebRequests(details) {
     let independentPathName = new URL(details.url).pathname.split('/').slice(2).join('/');
